22e2fca2f5386688223e6ed396c8d60676d95db0
JAParsons <johnparsons932@gmail.com>
2021-03-17 17:31:53 +0000

    Cleanup after babel fix
    Add comment to explain fix

--- babel-wrapper.cjs
+++ babel-wrapper.cjs
@@ -1,3 +1,10 @@
+// The babel mode needs to be set to 'upwards' so that it uses the root config
+// in every sub package. However, this is a babel runtime option and can't be
+// set in the babel config. We have to create a babelWrapper file that wraps
+// babel-jest, sets the rootMode and exports it.
+// Note that we use the .cjs extension, this is important to tell node that
+// its a common js file.
+// https://babeljs.io/docs/en/config-files#jest
 module.exports = require('babel-jest').createTransformer({
   rootMode: 'upward'
 });
--- yo.spec.js
+++
@@ -1,5 +0,0 @@
-// TESTING IF BABEL ACTUALLY WORKS
-
-import nodemon from 'nodemon';
-
-it('yo', () => {});
