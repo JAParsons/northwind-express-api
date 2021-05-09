73a665f83e8daa15ee0d97fe17ab852714cb7284
JAParsons <johnparsons932@gmail.com>
2021-05-09 20:06:05 +0100

    Add product query resolver

--- packages/express-server/src/graphql/resolvers.js
+++ packages/express-server/src/graphql/resolvers.js
@@ -1,3 +1,11 @@
+import { getProductById } from '@northwind/northwind-data';
+
 export const resolvers = {
-  Query: { hello: () => 'Hello world!' }
+  Query: {
+    hello: () => 'Hello world!',
+    product: async (_, { id }, { db }) => {
+      const [product] = await getProductById(db, id);
+      return product;
+    }
+  }
 };
